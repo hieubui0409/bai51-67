a6e9634701b23063849abdcf72274cefd31f66d7
hieubui0409 <hieub4680@gmail.com>
2024-07-10 19:09:07 +0700

    Create bai58.cs

---
+++ bai58.cs
@@ -0,0 +1,6 @@
+//Sử dụng kiểu dữ liệu float với hậu tố f
+float myFloat = 3.0f;
+// Sử dụng từ khóa var với giá trị có hậu tố f
+var myFloat = 3.0f;
+//Ép kiểu từ double sang float
+float myFloat = (float)3.0;
