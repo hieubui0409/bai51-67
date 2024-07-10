1a94d6adcf14eeb556730bbca8a4f8d687d7d938
hieubui0409 <hieub4680@gmail.com>
2024-07-10 19:02:51 +0700

    Create bai53.cs

---
+++ bai53.cs
@@ -0,0 +1,13 @@
+Trong đoạn mã này:
+
+Lớp Person:
+
+Có hai thuộc tính FirstName và LastName.
+Ghi đè phương thức Equals để so sánh hai đối tượng Person dựa trên FirstName và LastName.
+Ghi đè phương thức GetHashCode để tạo mã băm duy nhất cho các đối tượng Person dựa trên FirstName và LastName.
+Ghi đè phương thức ToString để trả về chuỗi đại diện của đối tượng Person.
+Lớp Program:
+
+Khởi tạo một Dictionary với khóa là Person và giá trị là string.
+Thêm một số phần tử vào Dictionary.
+Duyệt qua Dictionary và in ra các khóa và giá trị.
