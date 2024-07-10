33846bf6047180c46ad1ddc236b8122ae4f4b0c0
hieubui0409 <hieub4680@gmail.com>
2024-07-10 19:00:56 +0700

    Create bai52.cs

---
+++ bai52.cs
@@ -0,0 +1,5 @@
+rong đoạn mã này:
+
+Chúng ta khởi tạo một Dictionary với khóa là string và giá trị cũng là string.
+Chúng ta thêm ba phần tử vào Dictionary với các khóa "Nam", "Lan", và "Mai" cùng các giá trị tương ứng "1A", "2B", và "3C".
+Sử dụng vòng lặp foreach, chúng ta duyệt qua tất cả các phần tử trong Dictionary và in ra các khóa của chúng.
